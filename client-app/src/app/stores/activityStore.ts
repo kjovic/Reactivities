5d31e3af16ca5fc778d1bccdd8492275ae4993fa
Krešimir Lovro Jović <kresimir.jovic@adnet.hr>
2021-09-13 15:41:26 +0200

    End of Section 8

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,11 +1,11 @@
 import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
-import {v4 as uuid } from 'uuid';
+
 
 
 export default class ActivityStore{
-activityRegistry = new Map<string, Activity>();
+    activityRegistry = new Map<string, Activity>();
 selectedActivity: Activity | undefined = undefined;
 editMode = false;
 loading = false;
@@ -22,14 +22,12 @@ get activitiesByDate(){
 }
 
 loadActivities = async () => {
-
+this.loadingInitial = true;
  try{
 
     const activities = await agent.Activities.list();
-
         activities.forEach(activity => {
-            activity.date = activity.date.split('T')[0];
-            this.activityRegistry.set(activity.id, activity);
+           this.setActivity(activity);
 
         })
         this.setLoadingInitial(false);
@@ -43,34 +41,60 @@ loadActivities = async () => {
 
 
 }
+
+loadActivity = async (id: string)=>{
+
+    let activity = this.getActivity(id);
+    if(activity){
+        this.selectedActivity = activity;
+        return activity;
+    }else{
+    this.loadingInitial = true;
+    try{
+        activity = await agent.Activities.details(id);
+        this.setActivity(activity);
+        runInAction(()=>{
+            this.selectedActivity = activity;
+
+        })
+        
+        this.setLoadingInitial(false);
+        return activity;
+
+    }catch(error){
+     console.log(error);
+     this.setLoadingInitial(false);
+
+    }
+
+    }
+}
+
+private setActivity = (activity: Activity) => {
+
+    activity.date = activity.date.split('T')[0];
+    this.activityRegistry.set(activity.id, activity);
+
+}
+
+private getActivity = (id:string) => {
+    return this.activityRegistry.get(id);
+}
+
+
 setLoadingInitial = (state: boolean) => {
 this.loadingInitial = state;
 
 }
 
 
-selectActivity = (id: string) =>{
-    this.selectedActivity = this.activityRegistry.get(id);
-}
-cancelSelectedActivity = () => {
-    this.selectedActivity = undefined;
-}
 
-openForm = (id?: string) => {
-    id ? this.selectActivity(id) : this.cancelSelectedActivity();
-    this.editMode = true;
-}
-closeForm = () => {
-    this.editMode = false;
-}
 
 createActivity = async (activity: Activity) =>{
 
 this.loading = true;
-activity.id = uuid();
 
 try{
-
 await agent.Activities.create(activity);
 runInAction(()=>{
     this.activityRegistry.set(activity.id, activity);
@@ -127,7 +151,6 @@ updateActivity = async (activity: Activity) => {
          await agent.Activities.delete(id);
          runInAction(()=>{
             this.activityRegistry.delete(id);
-            if(this.selectedActivity?.id ===id) this.cancelSelectedActivity();
             this.loading = false;
          })
 
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,15 +1,23 @@
 import { observer } from 'mobx-react-lite';
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import { Link, useHistory, useParams } from 'react-router-dom';
 import { Button, Form, Segment } from 'semantic-ui-react';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { useStore } from '../../../app/stores/store';
+import {v4 as uuid } from 'uuid';
+
 
 
 
 export default observer( function ActivityForm(){
+    const history = useHistory();
     const {activityStore} = useStore();
-    const {selectedActivity, closeForm, createActivity, updateActivity,loading} = activityStore;
+    const {createActivity, updateActivity,loading, 
+        loadActivity, loadingInitial} = activityStore;
+    const {id} = useParams<{id:string}>();
 
-    const initalState = selectedActivity ?? {
+    const [activity, setActivity] = useState({
+
         id: '',
         title:'',
         category: '',
@@ -17,11 +25,26 @@ export default observer( function ActivityForm(){
         date:'',
         city:'',
         venue:''
-    }
+    });
 
-    const [activity, setActivity] = useState(initalState);
+    useEffect(()=>{
+
+        if (id) loadActivity(id).then(activity => setActivity(activity!))
+    }, [id, loadActivity]);
+
+
+
     function handleSubmit(){
-        activity.id ? updateActivity(activity) : createActivity(activity);
+        if (activity.id.length === 0) {
+            let newActivity = {
+                ...activity,
+                id: uuid()
+            };
+            createActivity(newActivity).then(()=>history.push(`/activities/${newActivity.id}`))
+        }
+        else{
+            updateActivity(activity).then(()=> history.push(`/activities/${activity.id}`))
+        }
 
         
     }
@@ -31,6 +54,7 @@ export default observer( function ActivityForm(){
     setActivity({...activity, [name]: value})
     }
 
+    if(loadingInitial)return <LoadingComponent content='Loading activity...'/>
  
 
 return(
@@ -43,7 +67,7 @@ return(
         <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange}/>
         <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange}/>
         <Button loading={loading} floated='right' positive type='submit' content='submit'/> 
-        <Button onClick={closeForm} floated='right' type='button' content='cancel'/> 
+        <Button as={Link} to='/activities' floated='right' type='button' content='cancel'/> 
     </Form>
 
 </Segment>
