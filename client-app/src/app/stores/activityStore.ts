f6e341300ac73fc4fb28b0c947deae25cfec87e7
Krešimir Lovro Jović <kresimir.jovic@adnet.hr>
2021-09-15 11:41:49 +0200

    End of Section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -21,6 +21,19 @@ get activitiesByDate(){
     Date.parse(a.date) -  Date.parse(b.date));
 }
 
+get GroupedActivities(){
+ return Object.entries(
+     this.activitiesByDate.reduce((activities, activity) => {
+         const date = activity.date;
+         activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+        return activities;
+
+     },{} as {[key:string]: Activity[]} )
+ )
+
+}
+
+
 loadActivities = async () => {
 this.loadingInitial = true;
  try{
